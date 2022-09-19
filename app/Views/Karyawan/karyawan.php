d5bd73e378bfb5e98892ffc30be5e7383b63096b
tomy3PL <112394993+Tomy3pl@users.noreply.github.com>
2022-09-19 15:53:43 +0700

    update comite

--- app/Views/Karyawan/karyawan.php
+++ app/Views/Karyawan/karyawan.php
@@ -7,8 +7,7 @@
 <?= $this->endsection('subjudul'); ?>
 <?= $this->section('isi'); ?>
 
-<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css"
-    rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
 
 
@@ -28,6 +27,7 @@
                     <th>No</th>
                     <th>ID User</th>
                     <th>Nama User</th>
+                    <th>Email</th>
                     <th>Level</th>
                     <th>Warehouse</th>
                     <th>Status</th>
@@ -45,26 +45,24 @@
                 }
                 foreach ($data as $row) :
                 ?>
-                <tr>
-                    <td><?= $no++; ?></td>
-                    <td><?= $row['id_user']; ?></td>
-                    <td><?= $row['nama_user']; ?></td>
-                    <td><?= $row['level'] ?></td>
-                    <td><?= $row['warehouse'] ?></td>
-                    <td>
-                        <button class="btn btn-sm <?= $row['status_kar'] == 1 ? "btn-success" : "btn-danger" ?>"
-                            onclick="status('<?= $row['id_user']; ?>')">
-                            <?= $row['status_kar'] == 1 ? "Aktif" : "No Aktif" ?>
-                        </button>
-                    </td>
-                    <td>
-                        <button href="#" class="btn btn-sm btn-info" onclick="edit('<?= $row['id_user']; ?>')"><i
-                                class="fa fa-pen-alt"></i>
-                        </button>
-                        <button class="btn btn-sm btn-danger" onclick="hapusUser('<?= $row['id_user']; ?>')"><i
-                                class="fa fa-trash-alt"></i>
-                    </td>
-                </tr>
+                    <tr>
+                        <td><?= $no++; ?></td>
+                        <td><?= $row['id_user']; ?></td>
+                        <td><?= $row['nama_user']; ?></td>
+                        <td><?= $row['email']; ?></td>
+                        <td><?= $row['level'] ?></td>
+                        <td><?= $row['warehouse'] ?></td>
+                        <td>
+                            <button class="btn btn-sm <?= $row['status_kar'] == 1 ? "btn-success" : "btn-danger" ?>" onclick="status('<?= $row['id_user']; ?>')">
+                                <?= $row['status_kar'] == 1 ? "Aktif" : "No Aktif" ?>
+                            </button>
+                        </td>
+                        <td>
+                            <button href="#" class="btn btn-sm btn-info" onclick="edit('<?= $row['id_user']; ?>')"><i class="fa fa-pen-alt"></i>
+                            </button>
+                            <button class="btn btn-sm btn-danger" onclick="hapusUser('<?= $row['id_user']; ?>')"><i class="fa fa-trash-alt"></i>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
@@ -74,139 +72,139 @@
 <div class="updateUser" style="display: none;"></div>
 
 <script>
-function status(users) {
-    Swal.fire({
-        title: 'Aktifkan/Noaktifkan',
-        text: 'Update sekarang ? ',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Update !'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                type: "post",
-                url: "<?= site_url() ?>Karyawan/UpdateStatus",
-                data: {
-                    users: users
-                },
-                dataType: "json",
-                success: function(response) {
-                    if (response.success) {
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Berhasil',
-                            text: response.success,
-                        }).then((result) => {
-                            if (result.isConfirmed) {
-                                window.location.href = (
-                                    '<?= site_url() ?>/Karyawan/index ');
-                            }
-                        });
-                        play_notif();
+    function status(users) {
+        Swal.fire({
+            title: 'Aktifkan/Noaktifkan',
+            text: 'Update sekarang ? ',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Update !'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: "post",
+                    url: "<?= site_url() ?>Karyawan/UpdateStatus",
+                    data: {
+                        users: users
+                    },
+                    dataType: "json",
+                    success: function(response) {
+                        if (response.success) {
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Berhasil',
+                                text: response.success,
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.href = (
+                                        '<?= site_url() ?>/Karyawan/index ');
+                                }
+                            });
+                            play_notif();
+                        }
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                        alert(xhr.status + '\n' + thrownError);
                     }
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    alert(xhr.status + '\n' + thrownError);
-                }
-            });
-        }
-    })
-
-}
-
-function edit(users) {
-    $.ajax({
-        type: "post",
-        url: "<?= site_url() ?>Karyawan/updateData",
-        data: {
-            code: users,
-        },
-        dataType: "json",
-        success: function(response) {
-
-            if (response.data) {
-                $('.updateUser').html(response.data).show();
-                $('#updateUser').modal('show');
-
+                });
             }
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-            alert(xhr.status + '\n' + thrownError);
-        }
-    });
-}
+        })
 
-function play_notif() {
-    var audio = document.createElement('audio');
-    audio.setAttribute('src', '<?= site_url() ?>/dist/img/success.mp3');
-    audio.setAttribute('autoplay', 'autoplay');
-    audio.play();
-    audio.load();
-}
+    }
 
-function hapusUser(iduser) {
-    Swal.fire({
-        title: 'Hapus Item',
-        text: "Yakin untuk hapus barang ? ",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Iya, Hapus!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                type: "post",
-                url: "<?= site_url('/Karyawan/deleteUser'); ?>",
-                data: {
-                    code: iduser
-                },
-                dataType: "json",
-                success: function(response) {
-                    if (response.success) {
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Berhasil',
-                            text: response.success,
-                        }).then((result) => {
-                            if (result.isConfirmed) {
-                                window.location.href = (
-                                    '<?= site_url() ?>/Karyawan/index ');
-                            }
-                        });
-                        play_notif();
-                    }
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    alert(xhr.status + '\n' + thrownError);
-                }
-            });
-        }
-    })
-}
-$(document).ready(function() {
-    $('#table1').DataTable();
-
-    $('.tambahData').click(function(e) {
-        e.preventDefault();
+    function edit(users) {
         $.ajax({
-            url: "<?= site_url('/Karyawan/modalTambah'); ?>",
-            // dataType: "json",
+            type: "post",
+            url: "<?= site_url() ?>Karyawan/updateData",
+            data: {
+                code: users,
+            },
+            dataType: "json",
             success: function(response) {
-                $('.tambahUsers').html(response).show();
-                $('#tambahUsers').on('show.bs.modal', function(event) {
-                    $('#users').focus();
-                })
-                $('#tambahUsers').modal('show');
+
+                if (response.data) {
+                    $('.updateUser').html(response.data).show();
+                    $('#updateUser').modal('show');
+
+                }
             },
             error: function(xhr, ajaxOptions, thrownError) {
                 alert(xhr.status + '\n' + thrownError);
             }
         });
-    });
+    }
 
-});
+    function play_notif() {
+        var audio = document.createElement('audio');
+        audio.setAttribute('src', '<?= site_url() ?>/dist/img/success.mp3');
+        audio.setAttribute('autoplay', 'autoplay');
+        audio.play();
+        audio.load();
+    }
+
+    function hapusUser(iduser) {
+        Swal.fire({
+            title: 'Hapus Item',
+            text: "Yakin untuk hapus barang ? ",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Iya, Hapus!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: "post",
+                    url: "<?= site_url('/Karyawan/deleteUser'); ?>",
+                    data: {
+                        code: iduser
+                    },
+                    dataType: "json",
+                    success: function(response) {
+                        if (response.success) {
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Berhasil',
+                                text: response.success,
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.href = (
+                                        '<?= site_url() ?>/Karyawan/index ');
+                                }
+                            });
+                            play_notif();
+                        }
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                        alert(xhr.status + '\n' + thrownError);
+                    }
+                });
+            }
+        })
+    }
+    $(document).ready(function() {
+        $('#table1').DataTable();
+
+        $('.tambahData').click(function(e) {
+            e.preventDefault();
+            $.ajax({
+                url: "<?= site_url('/Karyawan/modalTambah'); ?>",
+                // dataType: "json",
+                success: function(response) {
+                    $('.tambahUsers').html(response).show();
+                    $('#tambahUsers').on('show.bs.modal', function(event) {
+                        $('#users').focus();
+                    })
+                    $('#tambahUsers').modal('show');
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    alert(xhr.status + '\n' + thrownError);
+                }
+            });
+        });
+
+    });
 </script>
 <?= $this->endsection('isi'); ?>
