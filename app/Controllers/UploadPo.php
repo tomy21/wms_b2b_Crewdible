f5f4b7023e77113ce892ca518ce0117792cd6ddd
Tomy3pl <tomy@crew.id>
2022-10-18 23:06:28 +0700

    update comite

--- app/Controllers/UploadPo.php
+++ app/Controllers/UploadPo.php
@@ -89,6 +89,7 @@ class UploadPo extends BaseController
                         </div>'
                     ];
                     session()->setFlashdata($pesan_success);
+                    return;
                 } else {
                     $data = [
                         'nopo'          => $nopo,
