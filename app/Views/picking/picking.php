b1ad397c162ab64603c58498eae1fb7fb709f358
Tomy3pl <tomy@crew.id>
2022-10-26 21:53:27 +0700

    comite update

--- app/Views/picking/picking.php
+++ app/Views/picking/picking.php
@@ -33,9 +33,9 @@
                     $hari = 1;
                     $hariKemarin = date('Y-m-d', strtotime('-$hari day', strtotime($date)));
 
-                    $data1 = $db->table('tbl_picking')->where(['warehouse' => user()->warehouse, 'updated_at' => $hariKemarin])->orderBy('updated_at', 'DESC')->get()->getResultArray();
+                    $data1 = $db->table('tbl_picking')->where('warehouse', user()->warehouse)->orderBy('updated_at', 'DESC')->get()->getResultArray();
 
-                    $data2 = $db->table('tbl_picking')->where(['updated_at' => $hariKemarin])->orderBy('updated_at', 'DESC')->get()->getResultArray();
+                    $data2 = $db->table('tbl_picking')->orderBy('updated_at', 'DESC')->get()->getResultArray();
 
                     $data = user()->warehouse == 'Headoffice' ? $data2 : $data1;
                     foreach ($data as $query) :
