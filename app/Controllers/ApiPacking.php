82aad7ce33a7d0b35743f0ea097060b9a63762b7
Tomy3pl <tomy@crew.id>
2022-10-27 13:51:36 +0700

    update comite

--- app/Controllers/ApiPacking.php
+++ app/Controllers/ApiPacking.php
@@ -141,55 +141,11 @@ class ApiPacking extends ResourceController
     }
     public function updateFoto($id = null)
     {
-        $bucketName = 'crewdible-sandbox-asset';
-        $keyID      = 'AKIAUDN4SHKM45YIKVNS';
-        $keyScret   = 'Ne+n1vBDXP+DnAzZAAzqD3wh0KN7Jq2Snsk7KiW1';
         $modelPacking   = new PackingModel();
         $file = $this->request->getFile('foto');
         $file->move('./assets/uploades');
+
         $id = $this->request->getPost('id');
-
-        try {
-            // You may need to change the region. It will say in the URL when the bucket is open
-            // and on creation.
-            $s3 = S3Client::factory(
-                array(
-                    'credentials' => array(
-                        'key' => $keyID,
-                        'secret' => $keyScret
-                    ),
-                    'version' => 'latest',
-                    'region'  => 'us-east-2'
-                )
-            );
-        } catch (Exception $e) {
-            // We use a die, so if this fails. It stops here. Typically this is a REST call so this would
-            // return a json object.
-            die("Error: " . $e->getMessage());
-        }
-
-        // For this, I would generate a unqiue random string for the key name. But you can do whatever.
-        $keyName = 'test_example/' . $file;
-        $pathInS3 = 'https://s3.us-east-2.amazonaws.com/' . $bucketName . '/' . $keyName;
-
-        // Add it to S3
-        try {
-            // Uploaded:
-            $file = $_FILES["fileToUpload"]['tmp_name'];
-
-            $s3->putObject(
-                array(
-                    'Bucket' => $bucketName,
-                    'Key' =>  $keyName,
-                    'SourceFile' => $file,
-                    'StorageClass' => 'REDUCED_REDUNDANCY'
-                )
-            );
-        } catch (S3Exception $e) {
-            die('Error:' . $e->getMessage());
-        } catch (Exception $e) {
-            die('Error:' . $e->getMessage());
-        }
 
         $Order = $modelPacking->getWhere(['order_id' => $id])->getResultArray();
 
