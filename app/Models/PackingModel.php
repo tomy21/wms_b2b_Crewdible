1e79e70e7122908df3587dd10fbddf9e96f33cf0
tomy3PL <112394993+Tomy3pl@users.noreply.github.com>
2022-11-19 09:53:15 +0700

    Update PackingModel.php
    Update comite

--- app/Models/PackingModel.php
+++ app/Models/PackingModel.php
@@ -72,8 +72,8 @@ class PackingModel extends Model
         if ($warehouse == "Headoffice") {
             $tbl_storage = $this->dt->where(['created_at>=' => date('Y-m-d', strtotime('-1 days'))]);
         } else {
-            $tbl_storage = $this->dt->where(['stock_location' => $warehouse, 'created_at>=' => date('Y-m-d', strtotime('-1 days'))]);
-        }
+            $tbl_storage = $this->dt->where(['warehouse' => $warehouse, 'created_at>=' => date('Y-m-d', strtotime('-1 days'))]);
+    
         return $tbl_storage->countAllResults();
     }
 
