e2f61d8449190038a698bc44e3dfb95a5d877a8c
tomy3PL <112394993+Tomy3pl@users.noreply.github.com>
2022-11-19 11:21:37 +0700

    Update comite

--- app/Models/PackingModel.php
+++ app/Models/PackingModel.php
@@ -79,12 +79,7 @@ class PackingModel extends Model
 
     public function countAll()
     {
-        $warehouse = user()->warehouse;
-        if ($warehouse == "Headoffice") {
-            $tbl_storage = $this->dt->where(['created_at>=' => date('Y-m-d', strtotime('-1 days'))]);
-        } else {
-            $tbl_storage = $this->dt->where(['warehouse' => $warehouse,'created_at>=' => date('Y-m-d', strtotime('-1 days'))]);
-        }
+        $tbl_storage = $this->db->table($this->table);
         return $tbl_storage->countAllResults();
     }
 
