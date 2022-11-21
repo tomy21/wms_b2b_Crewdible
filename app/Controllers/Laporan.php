33c478d041ec9ae926c46ebd3761f7df128d26f2
Tomy3pl <tomy@crew.id>
2022-11-21 16:20:47 +0700

    update comite

--- app/Controllers/Laporan.php
+++ app/Controllers/Laporan.php
@@ -3,130 +3,195 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\InboundModel;
 use App\Models\InvoiceModel;
+use App\Models\ModelListHandover;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use App\Controllers\Xlsx;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use App\Models\ModelOrder;
 use App\Models\PackingModel;
 use App\Models\PickingModel;
+use App\Models\PoModel;
 use Config\Services;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx as WriterXlsx;
 
 class Laporan extends BaseController
 {
     public function index()
     {
-        $request = Services::request();
-        $modelOrder = new ModelOrder($request);
-        $modelPacking = new PackingModel($request);
-        $modelPicking = new PickingModel();
-        $tanggalAwal = $this->request->getVar('tglAwal');
-        $tanggalAkhir = $this->request->getVar('tglAkhir');
+        
 
-        if ($tanggalAwal == null && $tanggalAkhir == null) {
-            $order = $modelOrder->where('status', 5)->get();
-        } else {
-            $order = $modelOrder->where('created_at>=', $tanggalAwal)->where('created_at<=', $tanggalAkhir)->where('status', 7)->get();
+        return view('Laporan/index');
+    }
+    public function cetakReportInbound()
+    {
+        $tglawal        = $this->request->getPost('valAwalIn');
+        $tglakhir       = $this->request->getPost('valAkhirIn');
+
+        $tglakhir = date("Y-m-d", strtotime($tglakhir));
+        $tglawal = date("Y-m-d", strtotime($tglawal));
+
+        $request = Services::request();
+        $modelPO = new PoModel($request);
+        $query = $modelPO->where('created_at BETWEEN "' . $tglawal . '" AND "' . $tglakhir . '"')->get();
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', "Data Inbound $tglawal - $tglakhir");
+        $sheet->mergeCells('A1:E1');
+        $sheet->getStyle('A1')->getFont()->setBold(true);
+        $sheet->setCellValue('A3', "No");
+        $sheet->setCellValue('B3', "Warehouse");
+        $sheet->setCellValue('C3', "No PO");
+        $sheet->setCellValue('D3', "Total Qty");
+        $sheet->setCellValue('E3', "Total SKU");
+        $sheet->setCellValue('F3', "Nama Driver");
+        $sheet->setCellValue('G3', "No Pelat Kendaraan");
+        $sheet->setCellValue('H3', "Kedatangan Driver");
+        $sheet->setCellValue('I3', "Tanggal Input to stock");
+        $sheet->setCellValue('J3', "SLA");
+
+        $no = 1;
+        $column = 4;
+        foreach ($query->getResultArray() as $x) {
+            
+            //inbound 
+            $modelInbound = new InboundModel();
+            $jumlahQtyPO = $modelInbound->where(['nopo'=>$x['no_Po']])->countAllResults();
+
+            $date2 = date_create($x['waktu_datang']);
+            $dateFormat1 = date_format($date2, 'Y-m-d 23:59:00');
+            $dateFormat2 = date_format($date2, 'Y-m-d 18:00:00');
+            if ($x['waktu_datang'] <= $dateFormat2 && $x['updated_at'] > $dateFormat1) {
+                $sla = 'Over SLA';
+            } else {
+                $sla = 'Meet SLA';
+            }
+
+            $sheet->setCellValue('A' . $column, $no);
+            $sheet->setCellValue('B' . $column, $x['warehouse']);
+            $sheet->setCellValue('C' . $column, $x['no_Po']);
+            $sheet->setCellValue('D' . $column, $jumlahQtyPO);
+            $sheet->setCellValue('E' . $column, $x['quantity_item']);
+            $sheet->setCellValue('F' . $column, $x['driver']);
+            $sheet->setCellValue('G' . $column, $x['noplat']);
+            $sheet->setCellValue('H' . $column, $x['waktu_datang']);
+            $sheet->setCellValue('I' . $column, $x['updated_at']);
+            $sheet->setCellValue('J' . $column, $sla);
+            
+            $column++;
+            $no++;
         }
 
 
+        $writer = new Xlsx($spreadsheet);
+        $fileName = "Data Inbound $tglawal - $tglakhir";
 
-        $data = [
-            'data'  => $order,
-        ];
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename=' . $fileName . '.xlsx');
+        header('Cache-Control: max-age=0');
 
-
-        return view('Laporan/index', $data);
+        $writer->save('php://output');
     }
-    // public function cetakReportInbound()
-    // {
-    //     $tglawal        = $this->request->getPost('tglAwal');
-    //     $tglakhir       = $this->request->getPost('tglAkhir');
+    public function cetakReportOutbound()
+    {
+        $tglawal        = $this->request->getPost('valAwalOut');
+        $tglakhir       = $this->request->getPost('valAkhirOut');
 
-        // $modelBarang    = new InvoiceModel();
+        $tglakhir = date("Y-m-d", strtotime($tglakhir));
+        $tglawal = date("Y-m-d", strtotime($tglawal));
 
-        // $dataReport = $modelBarang->reportPeriode($tglawal, $tglakhir);
+        $request = Services::request();
+        $modalOrder = new ModelOrder($request);
+        $query = $modalOrder->where('created_at BETWEEN "'.$tglawal.'" AND "'.$tglakhir.'"')->get();
 
-
-    //     $spreadsheet = new Spreadsheet();
-    //     $sheet = $spreadsheet->getActiveSheet();
-    //     $sheet->setCellValue('A1', "Data Barang Masuk");
-    //     $sheet->mergeCells('A1:E1');
-    //     $sheet->getStyle('A1')->getFont()->setBold(true);
-    //     $sheet->setCellValue('A3', "No");
-    //     $sheet->setCellValue('B3', "Tanggal Masuk");
-    //     $sheet->setCellValue('C3', "No SKU");
-    //     $sheet->setCellValue('D3', "Nama Barang");
-    //     $sheet->setCellValue('E3', "Quantity");
-
-    //     $no = 1;
-    //     $numrow = 4;
-    //     foreach ($dataReport->getResultArray() as $row) :
-    //         $sheet->setCellValue('A' . $numrow, $no);
-    //         $sheet->setCellValue('B' . $numrow, $row['created_at']);
-    //         $sheet->setCellValue('C' . $numrow, $row['incodeSKU']);
-    //         $sheet->setCellValue('D' . $numrow, $row['innamaSKU']);
-    //         $sheet->setCellValue('E' . $numrow, $row['quantity']);
-
-    //         $no++;
-    //         $numrow++;
-    //     endforeach;
+        // var_dump($query->getResult());
+        // die;
 
 
-    //     $sheet->getDefaultRowDimension()->getRowHeight(-1);
-    //     $sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
-    //     $sheet->setTitle('Laporan Barang Masuk');
 
-    //     header('Content-Type : application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    //     header('Content-Disposition: attachment; filename = "LaporanBarangMasuk.xlsx"');
-    //     header('Cache-Control: max-age=0');
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', "Data Outbound $tglawal - $tglakhir");
+        $sheet->mergeCells('A1:E1');
+        $sheet->getStyle('A1')->getFont()->setBold(true);
+        $sheet->setCellValue('A3', "No");
+        $sheet->setCellValue('B3', "Warehouse");
+        $sheet->setCellValue('C3', "Order Id");
+        $sheet->setCellValue('D3', "Sum Quantity Order");
+        $sheet->setCellValue('E3', "Sum Quantity Packing");
+        $sheet->setCellValue('F3', "Count Items Order");
+        $sheet->setCellValue('G3', "Count Items Packing");
+        $sheet->setCellValue('H3', "Date Slot");
+        $sheet->setCellValue('I3', "Selesai Packing");
+        $sheet->setCellValue('J3', "Selesai Handover");
+        $sheet->setCellValue('K3', "SLA");
 
-    //     $writer = new Xlsx($spreadsheet);
-    //     $writer->save('php://output');
-    // }
-    // public function cetakReportOutbound()
-    // {
-    //     $tglawal        = $this->request->getPost('tglAwalOut');
-    //     $tglakhir       = $this->request->getPost('tglAkhirOut');
+        $no = 1;
+        $column = 4;
+        foreach ($query->getResultArray() as $x) {
+            // data invoice 
+            $modelInvoice = new InvoiceModel();
+            $count = $modelInvoice->where(['Order_id' => $x['Order_id']])->countAllResults();
+            $sumData = 0;
+            $idInv = null;
+            $query = $modelInvoice->where(['Order_id' => $x['Order_id']])->get()->getResult();
+            foreach ($query as $h) {
+                $sumData += $h->quantity;
+                $idInv = $h->id;
+            }
 
-    //     $modelOutbound = new InvoiceModel();
+            // data Packing
+            $modelPacking = new PackingModel($request);
+            $queryPacking = $modelPacking->where('order_id', $x['Order_id'])->get()->getResult();
+            $sumPacking = 0;
+            $countPacking = 0;
+            $datePacking = null;
+            foreach ($queryPacking as $z) {
+                foreach (json_decode($z->list) as $y) {
+                    $sumPacking += intval($y->quantity);
+                }
+                $jsonData = json_decode($z->list, true);
+                $countPacking = count($jsonData);
+                $datePacking = $z->updated_at;
+            }
 
-    //     $reportOut = $modelOutbound->reportPeriode($tglawal, $tglakhir);
-    //     $spreadsheet = new Spreadsheet();
-    //     $sheet = $spreadsheet->getActiveSheet();
-    //     $sheet->setCellValue('A1', "Data Barang Keluar");
-    //     $sheet->mergeCells('A1:E1');
-    //     $sheet->getStyle('A1')->getFont()->setBold(true);
-    //     $sheet->setCellValue('A3', "No");
-    //     $sheet->setCellValue('B3', "Tanggal Masuk");
-    //     $sheet->setCellValue('C3', "No SKU");
-    //     $sheet->setCellValue('D3', "Nama Barang");
-    //     $sheet->setCellValue('E3', "Quantity");
-
-    //     $no = 1;
-    //     $numrow = 4;
-    //     foreach ($reportOut->getResultArray() as $row) :
-    //         $sheet->setCellValue('A' . $numrow, $no);
-    //         $sheet->setCellValue('B' . $numrow, $row['created_at']);
-    //         $sheet->setCellValue('C' . $numrow, $row['codeSKU']);
-    //         $sheet->setCellValue('D' . $numrow, $row['namaSKU']);
-    //         $sheet->setCellValue('E' . $numrow, $row['quantity']);
-
-    //         $no++;
-    //         $numrow++;
-    //     endforeach;
+            // date Handover 
+            $modelHandover = new ModelListHandover();
+            $queryHandover = $modelHandover->getWhere(['order_id' => $x['Order_id']])->getResult();
+            $updatedHandover = null;
+            foreach ($queryHandover as $p) {
+                $updatedHandover = $p->updated_at;
+            }
 
 
-    //     $sheet->getDefaultRowDimension()->getRowHeight(-1);
-    //     $sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
-    //     $sheet->setTitle('Laporan Barang Keluar');
+            $sheet->setCellValue('A' . $column, $no);
+            $sheet->setCellValue('B' . $column, $x['stock_location']);
+            $sheet->setCellValue('C' . $column, $x['Order_id']);
+            $sheet->setCellValue('D' . $column, $sumData);
+            $sheet->setCellValue('E' . $column, $sumPacking);
+            $sheet->setCellValue('F' . $column, $count);
+            $sheet->setCellValue('G' . $column, $countPacking);
+            $sheet->setCellValue('H' . $column, $x['created_at']);
+            $sheet->setCellValue('I' . $column, $datePacking);
+            $sheet->setCellValue('J' . $column, $updatedHandover);
+            $sheet->setCellValue('K' . $column, $x['created_at'] <= $datePacking ? "OverSLA" : "MeetSLA");
 
-    //     header('Content-Type : application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    //     header('Content-Disposition: attachment; filename = "LaporanBarangKeluar.xlsx"');
-    //     header('Cache-Control: max-age=0');
+            $column++;
+            $no++;
+        }
+        
 
-    //     $writer = new Xlsx($spreadsheet);
-    //     $writer->save('php://output');
-    // }
+        $writer = new Xlsx($spreadsheet);
+        $fileName = "Data Outbound $tglawal - $tglakhir";
+
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename=' . $fileName . '.xlsx');
+        header('Cache-Control: max-age=0');
+
+        $writer->save('php://output');
+
+    }
     // function tampilGrafikBarangMasuk()
     // {
     //     $bulan = $this->request->getPost('bulan');
--- app/Views/Laporan/index.php
+++ app/Views/Laporan/index.php
@@ -15,23 +15,19 @@
                         Export Report Outbound
                     </div>
                     <div class="card-body">
-                        <?= form_open(site_url('Main/downloadOutbound')) ?>
+                        <?= form_open(site_url('Laporan/cetakReportOutbound')) ?>
                         <div class="form-group">
                             <label>Tanggal Mulai :</label>
                             <div class="input-group date" id="awal" data-target-input="nearest">
-                                <input type="text" class="form-control datetimepicker-input" data-target="#awal" name="valAwalOut" />
-                                <div class="input-group-append" data-target="#awal" data-toggle="datetimepicker">
-                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                </div>
+                                <input type="date" class="form-control" name="valAwalOut" />
+
                             </div>
                         </div>
                         <div class="form-group">
                             <label>Tanggal Akhir:</label>
                             <div class="input-group date" id="akhir" data-target-input="nearest">
-                                <input type="text" class="form-control datetimepicker-input" data-target="#akhir" name="valAkhirOut" />
-                                <div class="input-group-append" data-target="#akhir" data-toggle="datetimepicker">
-                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                </div>
+                                <input type="date" class="form-control" name="valAkhirOut" />
+
                             </div>
                         </div>
                         <div class="form-group">
@@ -47,28 +43,24 @@
                         Export Report Inbound
                     </div>
                     <div class="card-body">
+                        <?= form_open(site_url('Laporan/cetakReportInbound')) ?>
                         <div class="form-group">
                             <label>Tanggal Mulai :</label>
                             <div class="input-group date" id="awalIn" data-target-input="nearest">
-                                <input type="text" class="form-control datetimepicker-input" data-target="#awalIn" id="valAwalIn" />
-                                <div class="input-group-append" data-target="#awalIn" data-toggle="datetimepicker">
-                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                </div>
+                                <input type="date" class="form-control" name="valAwalIn" />
                             </div>
                         </div>
                         <div class="form-group">
                             <label>Tanggal Akhir:</label>
                             <div class="input-group date" id="akhirIn" data-target-input="nearest">
-                                <input type="text" class="form-control datetimepicker-input" data-target="#akhirIn" id="valAkhirIn" />
-                                <div class="input-group-append" data-target="#akhirIn" data-toggle="datetimepicker">
-                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                </div>
+                                <input type="date" class="form-control" name="valAkhirIn" />
                             </div>
                         </div>
                         <div class="form-group">
-                            <button class="btn btn-info" id="btnInbound"><i class="fa fa-download"></i> download data</button>
+                            <button class="btn btn-info" id="btnOutbound"><i class="fa fa-download"></i> download data</button>
                         </div>
                     </div>
+                    <?= form_close() ?>
                 </div>
             </div>
         </div>
@@ -83,39 +75,8 @@
             "buttons": ["copy", "csv", "excel", "pdf", "print"]
         }).buttons().container().appendTo('#report1 .col-md-6:eq(0)');
     });
-    $(document).ready(function() {
-        $('#awal').datetimepicker({
-            format: 'L'
-        });
-        $('#akhir').datetimepicker({
-            format: 'L'
-        });
-        $('#awalIn').datetimepicker({
-            format: 'L'
-        });
-        $('#akhirIn').datetimepicker({
-            format: 'L'
-        });
 
-        // $('#btnOutbound').click(function(e) {
-        //     e.preventDefault();
-        //     let awalOut = $('#valAwalOut').val();
-        //     let akhirOut = $('#valAkhirOut').val();
 
-        //     $.ajax({
-        //         type: "POST",
-        //         url: "<?= site_url('Main/downloadOutbound') ?>",
-        //         data: {
-        //             dateAwal:awalOut,
-        //             dateAkhir: akhirOut,
-        //         },
-        //         dataType: "JSON",
-        //         success: function (response) {
-
-        //         }
-        //     });
-
-        // });
-    });
+    
 </script>
 <?= $this->endsection('isi'); ?>
